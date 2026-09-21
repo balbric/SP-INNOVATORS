74e9e5e95950a3a91a95b96f918f9bf8f2a9a8ab
Balakrishnan R <balakrishnanrlkb2006@gmail.com>
2026-09-21 17:57:04 +0530

    Add Mac Number Identification code for Esp32

---
+++ Mac Number Identification Code.ino
@@ -0,0 +1,27 @@
+#include <WiFi.h>
+#include "esp_wifi.h"
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  WiFi.mode(WIFI_STA);
+  delay(100);
+
+  uint8_t mac[6];
+  esp_wifi_get_mac(WIFI_IF_STA, mac);
+
+  Serial.println();
+  Serial.println("ESP32 MAC ADDRESS:");
+
+  for (int i = 0; i < 6; i++) {
+    if (mac[i] < 16) Serial.print("0");
+    Serial.print(mac[i], HEX);
+    if (i < 5) Serial.print(":");
+  }
+
+  Serial.println();
+}
+
+void loop() {
+}
